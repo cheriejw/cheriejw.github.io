37c6f2e4dd9afdf381b51e2727c0e25a204042e3
Cherie Woo <cheriejawoo@yahoo.com>
2022-08-12 00:03:32 -0700

    Add behavior when nav bar goes below home section and some clean up...

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Config } from '../shared/config';
 
 @Component({
   selector: 'app-nav',
@@ -6,14 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
   PORTFOLIO_NAME: string;
+  COMPONENT_HEIGHT: string;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    // When you scroll past the home component...
+    if (number > Config.homeComponentHeight - Config.navComponentHeight) {
+      document.getElementById('nav_component').classList.add('past-home-component');
+      document.getElementById('nav_component').classList.remove('over-home-component');
+    } else {
+      document.getElementById('nav_component').classList.add('over-home-component');
+      document.getElementById('nav_component').classList.remove('past-home-component');
+    }
+  }
 
   constructor() {
-    this.PORTFOLIO_NAME = 'Cherie Woo';
+    this.PORTFOLIO_NAME = Config.portfolioName;
+    this.COMPONENT_HEIGHT = Config.navComponentHeight + 'px';
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
